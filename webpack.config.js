8dcb4ff6f30ce2d96ea20381462bd218a8864bf1
Viewpit <viewpit@hotmail.no>
2017-10-22 18:26:05 +0200

    Add project files.

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+//const CompressionPlugin = require('compression-webpack-plugin');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const { AureliaPlugin } = require('aurelia-webpack-plugin');
+const bundleOutputDir = './wwwroot/dist';
+
+module.exports = (env) => {
+    let isDevBuild = !(env && env.prod);
+    return [{
+        stats: { modules: false },
+        entry: { 'app': 'aurelia-bootstrapper' },
+        resolve: {
+            extensions: ['.ts', '.js'],
+            modules: ['ClientApp', 'node_modules'],
+        },
+        output: {
+            path: path.resolve(bundleOutputDir),
+            publicPath: 'dist/',
+            filename: '[name].js'
+        },
+        module: {
+            rules: [
+                { test: /\.ts$/i, include: /ClientApp/, use: 'ts-loader?silent=true' },
+                { test: /\.html$/i, use: 'html-loader' },
+                { test: /\.css$/i, use: isDevBuild ? 'css-loader' : 'css-loader?minimize' },
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            ]
+        },
+        plugins: [
+            new webpack.DefinePlugin({ IS_DEV_BUILD: JSON.stringify(isDevBuild) }),
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./wwwroot/dist/vendor-manifest.json')
+            }),
+            new AureliaPlugin({ aureliaApp: 'boot' })
+        ].concat(
+            //isDevBuild ? [
+            //new webpack.SourceMapDevToolPlugin({
+            //    filename: '[file].map', // Remove this line if you prefer inline source maps
+            //    moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]')  // Point sourcemap entries to the original file locations on disk
+            //    })] :
+            [
+                //new BundleAnalyzerPlugin(),
+            
+            new webpack.optimize.UglifyJsPlugin(
+            {
+              compress: { warnings: false },
+              output: { comments: false, beautify: false },
+              ecma: 8 })          
+          //,new CompressionPlugin({ test: /\.js/ })
+        ])
+    }];
+}
